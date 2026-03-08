9a148785a9889d42c6fe4676d2c017f965640be9
dodam24 <dodam0724@gmail.com>
2026-03-08 12:31:23 +0900

    docs(webpack): webpack 시작하기 튜토리얼 - 소스 분석

--- getting-started/src/index.js
+++ getting-started/src/index.js
@@ -1,10 +1,17 @@
+// ES6 모듈 시스템을 사용하여 lodash를 가져옴
 import _ from "lodash";
+// ES5 모듈 시스템을 사용하여 lodash를 가져옴
+// var _ = require("lodash");
+
+// CommonJS 모듈 시스템을 사용하여 path 모듈을 가져옴 (ES5)
+var path = require("path");
 
 function component() {
     var element = document.createElement("div");
 
-    /* lodash is required for the next line to work */
+    // lodash의 join 함수를 사용하여 문자열을 생성
     element.innerHTML = _.join(["Hello", "webpack"], " ");
+    // element.innerHTML = "Hello webpack";
 
     return element;
 }
