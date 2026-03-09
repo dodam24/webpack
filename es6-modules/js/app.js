36a422286cf4b078d38f9c773b6b847ba2f5e833
dodam24 <dodam0724@gmail.com>
2026-03-09 21:33:38 +0900

    docs(babel): Babel과 ES6 Modules 문법 정리

---
+++ es6-modules/js/app.js
@@ -0,0 +1,4 @@
+// math.js 파일에서 두 숫자의 합을 구하는 sum 함수를 import하여 콘솔에 출력
+
+import { sum } from "./math";
+console.log("10 + 20 = ", sum(10, 20));
---
+++ es6-modules/js/math.js
@@ -0,0 +1,5 @@
+// sum() 함수 구현 후 export
+
+export function sum(a, b) {
+    return a + b;
+}
